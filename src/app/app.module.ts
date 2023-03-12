860a698be4bbfdfe4f2c4856d412e70c1d70df89
Camilo Gálvez Vidal <c.galvezv@uniandes.edu.co>
2023-03-12 09:25:38 -0300

    Add angular material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ClockComponent } from './components/clock/clock.component';
+import { ChallengeQuestionComponent } from './components/challenge-question/challenge-question.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent,
+    HomeComponent,
+    ClockComponent,
+    ChallengeQuestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
